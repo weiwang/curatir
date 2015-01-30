ca3561e36b19aec7c4a0860e34920af3b8f83dcf
Wei Wang <thomas.wei.wang@gmail.com>
2015-01-30 04:29:13 -0500

    updated host ip

--- data/artworks/create_mysql-db.py
+++ data/artworks/create_mysql-db.py
@@ -10,9 +10,6 @@ with con:
     cur = con.cursor()
     cur.execute("DROP TABLE IF EXISTS artworks")
     cur.execute("CREATE TABLE artworks (Id INT PRIMARY KEY AUTO_INCREMENT,TITLE VARCHAR(50),ARTIST VARCHAR(50), MATERIAL VARCHAR(50), VENUE VARCHAR(50), URL VARCHAR(100), IMAGE_URL VARCHAR(100))")
-
-    ## add_artwork = ("INSERT INTO artworks (TITLE, ARTIST, MATERIAL, VENUE, URL, IMAGE_URL) VALUES (%s, %s, %s, %s, %s, %s)")        
-    
     for file in files:
         museum = json.load(open(file))
         for record in museum:            
@@ -24,21 +21,10 @@ with con:
                 url = record['id']
                 if type(url) is list:
                     url = url[0]
-                #url.replace("'", "''")
                 image_url = record['image']
                 if type(image_url) is list:
                     image_url = image_url[0]
-                #image_url.replace("'", "''")
                 query = u"INSERT INTO artworks (TITLE, ARTIST, MATERIAL, VENUE, URL, IMAGE_URL) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')".format(title, artist, material, venue, url, image_url)
                 cur.execute(query)
-                #cur.execute(add_artwork, artwork)
 
-# with con: 
-#     cur = con.cursor()
-#     cur.execute("SELECT * FROM artworks")
-#     rows = cur.fetchall()
-#     for row in rows:
-#         print row
-                
-# cur.close()
-# con.close()
+
